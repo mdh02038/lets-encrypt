1f2c1d5d96e5c53d3bae7aeae70e0a3de078dd75
mdh02038 <mdh@raquette.com>
2016-06-17 14:50:37 -0400

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+/certbot $args
+
+if [[ -n $CRONTAB ]]; then
+  args="$@"
+  echo "${CRON_OPT//\"/} root \
+	/certbot $args >> /var/log/cron.log 2>&1" > /etc/cron.d/certbot
+    chmod 0644 /etc/cron.d/certbot
+    touch /var/log/cron.log
+    cron && tail -f /var/log/cron.log;
+fi
+
+
+
