10bc7d80b08efad75a1a33da58278888b821eb9c
mdh02038 <mdh@raquette.com>
2016-06-17 15:33:16 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-/opt/certbot/venv/bin/certbot $args
+/opt/certbot/venv/bin/certbot $@
 
 if [[ -n $CRONTAB ]]; then
-  args="$@"
   echo "${CRON_OPT//\"/} root \
-	/opt/certbot/venv/bin/certbot $args >> /var/log/cron.log 2>&1" > /etc/cron.d/certbot
+	/opt/certbot/venv/bin/certbot $@ >> /var/log/cron.log 2>&1" > /etc/cron.d/certbot
     chmod 0644 /etc/cron.d/certbot
     touch /var/log/cron.log
     cron && tail -f /var/log/cron.log;
